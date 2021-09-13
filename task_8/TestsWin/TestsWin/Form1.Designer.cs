10d32a28003d7ca96104022d545540771d3d492f
Grace <kalinina.grace@gmail.com>
2021-09-13 10:51:45 +0300

    Task 8 add.

---
+++ task_8/TestsWin/TestsWin/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace TestsWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_normal = new System.Windows.Forms.Button();
+            this.btn_normal_parallel = new System.Windows.Forms.Button();
+            this.btn_async = new System.Windows.Forms.Button();
+            this.btn_parallel_async = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelMethod = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_normal
+            // 
+            this.btn_normal.Location = new System.Drawing.Point(183, 29);
+            this.btn_normal.Name = "btn_normal";
+            this.btn_normal.Size = new System.Drawing.Size(403, 31);
+            this.btn_normal.TabIndex = 0;
+            this.btn_normal.Text = "Normal Execute";
+            this.btn_normal.UseVisualStyleBackColor = true;
+            this.btn_normal.Click += new System.EventHandler(this.btn_normal_Click);
+            // 
+            // btn_normal_parallel
+            // 
+            this.btn_normal_parallel.Location = new System.Drawing.Point(183, 78);
+            this.btn_normal_parallel.Name = "btn_normal_parallel";
+            this.btn_normal_parallel.Size = new System.Drawing.Size(403, 33);
+            this.btn_normal_parallel.TabIndex = 1;
+            this.btn_normal_parallel.Text = "Normal Parallel Execute";
+            this.btn_normal_parallel.UseVisualStyleBackColor = true;
+            this.btn_normal_parallel.Click += new System.EventHandler(this.btn_normal_parallel_Click);
+            // 
+            // btn_async
+            // 
+            this.btn_async.Location = new System.Drawing.Point(183, 129);
+            this.btn_async.Name = "btn_async";
+            this.btn_async.Size = new System.Drawing.Size(402, 33);
+            this.btn_async.TabIndex = 4;
+            this.btn_async.Text = "Async Execute";
+            this.btn_async.UseVisualStyleBackColor = true;
+            this.btn_async.Click += new System.EventHandler(this.btn_async_Click);
+            // 
+            // btn_parallel_async
+            // 
+            this.btn_parallel_async.Location = new System.Drawing.Point(183, 181);
+            this.btn_parallel_async.Name = "btn_parallel_async";
+            this.btn_parallel_async.Size = new System.Drawing.Size(402, 32);
+            this.btn_parallel_async.TabIndex = 6;
+            this.btn_parallel_async.Text = "Parallel Async Execute";
+            this.btn_parallel_async.UseVisualStyleBackColor = true;
+            this.btn_parallel_async.Click += new System.EventHandler(this.btn_parallel_async_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(38, 294);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(744, 401);
+            this.textBox.TabIndex = 8;
+            // 
+            // labelMethod
+            // 
+            this.labelMethod.AutoSize = true;
+            this.labelMethod.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelMethod.Location = new System.Drawing.Point(38, 225);
+            this.labelMethod.Name = "labelMethod";
+            this.labelMethod.Size = new System.Drawing.Size(61, 20);
+            this.labelMethod.TabIndex = 9;
+            this.labelMethod.Text = "Method";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 740);
+            this.Controls.Add(this.labelMethod);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btn_parallel_async);
+            this.Controls.Add(this.btn_async);
+            this.Controls.Add(this.btn_normal_parallel);
+            this.Controls.Add(this.btn_normal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_normal;
+        private System.Windows.Forms.Button btn_normal_parallel;
+        private System.Windows.Forms.Button btn_async;
+        private System.Windows.Forms.Button btn_parallel_async;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelMethod;
+    }
+}
+
---
+++ task_8/TestsWin/TestsWin/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestsWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //results 
+        string res;
+        //websites to be downloaded(their index page)
+        public static List<string> websites = new List<string>()
+        {
+            "https://www.google.com",
+            "https://www.microsoft.com",
+            "https://www.cnn.com",
+            "https://www.amazon.com",
+            "https://www.facebook.com",
+            "https://www.twitter.com",
+            "https://www.codeproject.com",
+            "https://www.stackoverflow.com",
+            "https://en.wikipedia.org/wiki/.NET_Framework",
+            "https://nakov.com",
+            "https://elmah.io",
+            "https://www.pluralsight.com",
+            "https://www.udemy.com"
+        };
+        //method to download one site
+        private static WebsiteDataModel DownloadWebsite(string websiteUrl)
+        {
+            WebsiteDataModel result = new WebsiteDataModel();
+
+            result.WebsiteUrl = websiteUrl;
+            WebClient webClient = new WebClient();
+            //DownloadString has an async method
+            result.WebsiteData = webClient.DownloadString(websiteUrl);
+            //returning WebsiteDataModel obj(Url,result)
+            return result;
+        }
+
+        //normal execute click event
+        //normal aka sync
+        private void btn_normal_Click(object sender, EventArgs e)
+        {
+            res = "";
+            labelMethod.Text = "Normal";
+            Stopwatch sw = new();
+            sw.Start();
+            DownloadNormal();
+            sw.Stop();
+            var time = sw.ElapsedMilliseconds;
+            string conclusion = "Total time: " + time + "milliseconds.";
+            //result display needs opt
+            res += conclusion;
+            textBox.Text = res;
+        }
+        //async execute click event
+        private async void btn_async_Click(object sender, EventArgs e)
+        {
+            res = "";
+            labelMethod.Text = "Async";
+            Stopwatch sw = new();
+            sw.Start();
+            await DownloadAsync();
+            sw.Stop();
+            var time = sw.ElapsedMilliseconds;
+            string conclusion = "Total time: " + time + "milliseconds.";
+            //result display needs opt
+            res += conclusion;
+            textBox.Text = res;
+        }
+        //parallel async click event
+        private async void btn_parallel_async_Click(object sender, EventArgs e)
+        {
+            res = "";
+            labelMethod.Text = "Async Parallel";
+            Stopwatch sw = new();
+            sw.Start();
+            await DownloadParallelAsync();
+            sw.Stop();
+            var time = sw.ElapsedMilliseconds;
+            string conclusion = "Total time: " + time + "milliseconds.";
+            //result display needs opt
+            res += conclusion;
+            textBox.Text = res;
+        }
+        //parallel normal click event
+        private void btn_normal_parallel_Click(object sender, EventArgs e)
+        {
+            res = "";
+            labelMethod.Text = "Normal Parallel";
+            Stopwatch sw = new();
+            sw.Start();
+            DownloadParallelNormal();
+            sw.Stop();
+            var time = sw.ElapsedMilliseconds;
+            string conclusion = "Total time: " + time + "milliseconds.";
+            //result display needs opt
+            res += conclusion;
+            textBox.Text = res;
+        }
+        //METHODS
+        //Normal download site
+        private void DownloadNormal()
+        {
+            List<string> urls = websites;
+            foreach (string website in urls)
+            {
+                WebsiteDataModel result = DownloadWebsite(website);
+                ReportResult(result);
+            }
+        }
+        //Async download site
+        private async Task DownloadAsync()
+        {
+            List<string> urls = websites;
+            foreach (string website in urls)
+            {
+                //if we can't change the DownloadWebsite method to an async one
+                //we're waiting for the website to download
+                //waiting time-sum of all download times
+                WebsiteDataModel result = await Task.Run(()=>DownloadWebsite(website));
+                ReportResult(result);
+            }
+        }
+        //Parallel async download site
+        private async Task DownloadParallelAsync()
+        {
+            List<string> urls = websites;
+            List<Task<WebsiteDataModel>> taskItems = new List<Task<WebsiteDataModel>>();
+
+            foreach (string website in urls)
+            {
+                //returning Task<WebsiteDataModel> if not awaited as in this case
+                //we're adding the downloading task to the list even if not done yet
+                //---meaning all tasks are running at the same time
+                //the downloading of these tasks is executed in parallel(at the same time)
+                //---but also async because each task is not waiting for the one before to be completed
+                //waiting time: the time of the task with the longest time
+                //here we don't need the Parallel class simply because we do not block the thread(as in sync)
+                taskItems.Add(Task.Run(() => DownloadWebsite(website)));
+            }
+            //passing a list of task and waiting until all of them are done
+            var results = await Task.WhenAll(taskItems);
+            foreach(WebsiteDataModel t in results)
+            {
+                ReportResult(t);
+            }
+        }
+        //Parallel normal download site
+        private void DownloadParallelNormal()
+        {
+            List<string> urls = websites;
+            List<WebsiteDataModel> items = new List<WebsiteDataModel>();
+            //the Parallel class executes the selected operation(in this case ForEach) in parallel
+            //---meaning that it doesn't wait for each loop to be done executing before firing the next one
+            //although the Downloading of the website is still sync meaning that a Task is fired and the thread is blocked
+            //--- if needed the Parallel class(the threadpool) can create new threads to deal with it
+            //conceptually the threads are often said to run at the same time(in parallel),
+            //---they are actually running consecutively in time slices 
+            //closer look->Parallel schedules tasks to TaskScheduler, the scheduler then goes to the ThreadPool 
+            Parallel.ForEach<string>(urls, (website) => {
+                items.Add(DownloadWebsite(website));
+            });
+            foreach(WebsiteDataModel t in items)
+            {
+                ReportResult(t);
+            }
+        }
+
+        //reporting the results
+        private void ReportResult(WebsiteDataModel result)
+        {
+            string resultString = $"Site " + result.WebsiteUrl + " - " + result.WebsiteData.Length + " characters long downloaded" + Environment.NewLine;
+            res += resultString;
+
+        }
+
+        
+    }
+}
